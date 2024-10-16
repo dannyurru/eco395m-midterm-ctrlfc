c5df65fc34a1a617f7e9894476bd2c34940078a0
Nicolas Salas <nms3359@my.utexas.edu>
2024-10-15 21:31:20 -0500

    Fixed Quotes

--- scraping_code/la_liga.py
+++ scraping_code/la_liga.py
@@ -15,7 +15,7 @@ headers = {
         "Cookie": "SWID=75534310-463B-4EC6-CE90-8370E807F123;dcf=1; connectionspeed=full; country=us; edition=espn-en-us; edition-view=espn-en-us; region=ccpa"
         }
 
-with open(OUTPUT_PATH, 'w', newline='', encoding='utf-8') as file:
+with open(OUTPUT_PATH, "w", newline="", encoding="utf-8") as file:
     writer = csv.writer(file)
     writer.writerow(["Standing" ,"Team Name","Games Played", "Wins", "Draws", "Losses", "Goals For", "Goals Against", "Goal Difference", "Points", "Year"])
 
@@ -37,7 +37,7 @@ with open(OUTPUT_PATH, 'w', newline='', encoding='utf-8') as file:
             stats_for_team = stats[i*8:(i+1)*8]
             writer.writerow([standings[i], team] + stats_for_team + [year])
 
-with open(OUTPUT_PATH, 'r', encoding='utf-8') as file:
+with open(OUTPUT_PATH, "r", encoding="utf-8") as file:
     reader = csv.reader(file)
     header = next(reader)
     data = [row for row in reader]
@@ -57,7 +57,7 @@ for year, rows in yearly_data.items():
         point = int(row[9])
         goal_differences.append(goal_diff)
         points.append(point)
-
+        
     min_goal_diff = min(goal_differences) - 1
     min_points = min(points) - 1
 
@@ -70,7 +70,7 @@ for year, rows in yearly_data.items():
         rts = ((goal_diff - min_goal_diff) / std_goal_diff) * ((point - min_points) / std_points)
         row.append(rts)
 
-with open(OUTPUT_PATH, 'w', newline='', encoding='utf-8') as file:
+with open(OUTPUT_PATH, "w", newline="", encoding="utf-8") as file:
     writer = csv.writer(file)
     writer.writerow(header + ["RTS"])
     for year, rows in yearly_data.items():
